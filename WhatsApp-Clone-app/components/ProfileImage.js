5e2c931f0a24f55a4cba867b15d0033a36779dee
Gvn <gvanca.uplisashvili@gmail.com>
2023-09-18 16:04:15 +0400

    ..something went wrong

--- WhatsApp-Clone-app/components/ProfileImage.js
+++ WhatsApp-Clone-app/components/ProfileImage.js
@@ -3,39 +3,19 @@ import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 import userImage from "../assets/images/userImage.png";
 import colors from "../constants/colors";
 import { FontAwesome } from "@expo/vector-icons";
-<<<<<<< HEAD
-import {
-  launchImagePicker,
-  uploadImageAsync,
-} from "../utils/imagePickerHelper";
-import { updateSignedInUserData } from "../utils/actions/authActions";
-import { getFirestore, doc, updateDoc } from "firebase/firestore";
-=======
 import { launchImagePicker } from "../utils/imagePickerHelper";
->>>>>>> parent of 38eb924 (Uploading images)
 
 const ProfileImage = (props) => {
   const source = props.uri ? { uri: props.uri } : userImage;
   const [image, setImage] = useState(source);
-  const userId = props.userId;
 
   const pickImage = async () => {
     try {
       const tempUri = await launchImagePicker();
 
       if (!tempUri) return;
-<<<<<<< HEAD
-      const uploadUrl = await uploadImageAsync(tempUri);
-      if (!uploadUrl) throw new Error("Could not upload image");
-      await updateSignedInUserData(userId, { ProfilePicture: uploadUrl });
-      setImage({ uri: uploadUrl });
-    } catch (error) {
-      console.error(error);
-    }
-=======
       setImage({ uri: tempUri });
     } catch (error) {}
->>>>>>> parent of 38eb924 (Uploading images)
   };
 
   return (
