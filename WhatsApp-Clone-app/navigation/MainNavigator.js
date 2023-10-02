0fa29a1c493f44acd277ef346f4395a8f75d2030
Gvn <gvanca.uplisashvili@gmail.com>
2023-10-02 14:35:22 +0400

    Chat
    Getting the user chats when the app starts

--- WhatsApp-Clone-app/navigation/MainNavigator.js
+++ WhatsApp-Clone-app/navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigator from "./TabNavigator";
 import ChatSettingsScreen from "../Screens/ChatSettingsScreen";
@@ -7,10 +7,31 @@ import NewChatScreen from "../Screens/NewChatScreen";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import colors from "../constants/colors";
+import { useSelector } from "react-redux";
+import getFirebaseApp from "../utils/firebaseHelper";
+import { child, getDatabase, onValue, ref } from "firebase/database";
 
 const Stack = createNativeStackNavigator();
 
-const MainNavigator = () => {
+const MainNavigator = (props) => {
+  const userData = useSelector((state) => state.auth.userData);
+  const storedUsers = useSelector((state) => state.users.storedUsers);
+
+  useEffect(() => {
+    console.log("Subscribing to firebase listeners");
+
+    const app = getFirebaseApp();
+    const dbRef = ref(getDatabase(app));
+    const userChatsRef = child(dbRef, `userChats/${userData.userId}`);
+
+    onValue(userChatsRef, (querySnapshot) => {
+      const chatIdsData = querySnapshot.val() || {};
+      const chatIds = Object.values(chatIdsData);
+
+      console.log(chatIds);
+    });
+  }, []);
+
   const navigation = useNavigation();
 
   return (
@@ -20,8 +41,10 @@ const MainNavigator = () => {
           name="Home"
           component={TabNavigator}
           options={{
-            headerTitle: "",
-            headerShadowVisible: false,
+            // headerTitle: "",
+            // headerShadowVisible: false,
+            // headerBackTitleVisible: false,
+            headerShown: false,
           }}
         />
         <Stack.Screen
